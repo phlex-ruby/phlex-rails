4e6da0b197688a23e74a1cd2ed28d41dd2209a58
Joel Drapper <joel@drapper.me>
2022-11-24 19:49:11 +0000

    Improve controller generator routing

--- lib/generators/phlex/controller/controller_generator.rb
+++ lib/generators/phlex/controller/controller_generator.rb
@@ -5,9 +5,19 @@ module Phlex
 		class ControllerGenerator < ::Rails::Generators::NamedBase # :nodoc:
 			source_root File.expand_path("templates", __dir__)
 
-			argument :actions, type: :array, default: [], banner: "action action"
-			class_option :skip_routes, type: :boolean, desc: "Don't add routes to config/routes.rb."
-			class_option :parent, type: :string, default: "ApplicationController", desc: "The parent class for the generated controller"
+			argument :actions,
+				type: :array,
+				default: [],
+				banner: "action action"
+
+			class_option :skip_routes,
+				type: :boolean,
+				desc: "Don't add routes to config/routes.rb."
+
+			class_option :parent,
+				type: :string,
+				default: "ApplicationController",
+				desc: "The parent class for the generated controller"
 
 			check_class_collision suffix: "Controller"
 
@@ -20,15 +30,19 @@ module Phlex
 				empty_directory base_path
 
 				actions.each do |action|
-					Rails::Generators.invoke("phlex:view", [remove_possible_suffix(name) + "/" + action])
+					::Rails::Generators.invoke("phlex:page", [name + "/" + action])
 				end
 			end
 
 			def add_routes
 				return if options[:skip_routes]
-				return if actions.empty?
 
-				routing_code = actions.map { |action| "get '#{file_name}/#{action}'" }.join("\n")
+				routing_code = "resources :#{file_name}"
+
+				if actions.any?
+					routing_code << ", only: [#{actions.map { ":#{_1}" }.join(', ')}]"
+				end
+
 				route routing_code, namespace: regular_class_path
 			end
 
@@ -43,7 +57,11 @@ module Phlex
 			end
 
 			def file_name
-				@_file_name ||= remove_possible_suffix(super)
+				remove_possible_suffix(super)
+			end
+
+			def name
+				remove_possible_suffix(super)
 			end
 
 			def remove_possible_suffix(name)
