d46682c90a5b112f8f2bac4d8ef8383592df4041
Joel Drapper <joel@drapper.me>
2023-01-31 12:49:20 +0000

    Refactor modules

---
+++ lib/phlex/rails/html/append_method_added_warning.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Phlex
+	module Rails
+		module HTML
+			module AppendMethodAddedWarning
+				def method_added(name)
+					if name == :append || name == :safe_append
+						raise Phlex::NameError, "You shouldn't redefine the #{name} method as it's required for safe HTML output."
+					end
+
+					super
+				end
+			end
+		end
+	end
+end
---
+++ lib/phlex/rails/html/class_methods.rb
@@ -0,0 +1,11 @@
+module Phlex
+	module Rails
+		module HTML
+		  module ClassMethods
+				def render_in(...)
+					new.render_in(...)
+				end
+			end
+		end
+	end
+end
