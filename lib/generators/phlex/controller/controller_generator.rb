66030d2923cb5f92efa12c647c4b90e8afc2d2b4
Joel Drapper <joel@drapper.me>
2023-02-13 14:54:21 +0000

    Improved generators

--- lib/generators/phlex/controller/controller_generator.rb
+++ lib/generators/phlex/controller/controller_generator.rb
@@ -1,72 +1,74 @@
 # frozen_string_literal: true
 
-module Phlex
-	module Generators
-		class ControllerGenerator < ::Rails::Generators::NamedBase # :nodoc:
-			source_root File.expand_path("templates", __dir__)
+module Phlex::Generators
+	class ControllerGenerator < ::Rails::Generators::NamedBase # :nodoc:
+		source_root File.expand_path("templates", __dir__)
 
-			argument :actions,
-				type: :array,
-				default: [],
-				banner: "action action"
+		check_class_collision suffix: "Controller"
 
-			class_option :skip_routes,
-				type: :boolean,
-				desc: "Don't add routes to config/routes.rb."
+		argument :actions,
+			type: :array,
+			default: [],
+			banner: "action action"
 
-			class_option :parent,
-				type: :string,
-				default: "ApplicationController",
-				desc: "The parent class for the generated controller"
+		class_option :skip_routes,
+			type: :boolean,
+			desc: "Don't add routes to config/routes.rb."
 
-			check_class_collision suffix: "Controller"
+		class_option :parent,
+			type: :string,
+			default: "ApplicationController",
+			desc: "The parent class for the generated controller"
 
-			def create_controller_files
-				template "controller.rb.erb", File.join("app/controllers", class_path, "#{file_name}_controller.rb")
+		def create_controllers
+			template "controller.rb.erb", File.join(
+				"app/controllers", class_path, "#{file_name}_controller.rb"
+			)
+		end
+
+		def copy_views
+			actions.each do |action|
+				@action = action
+				@path = File.join(
+					"app/views", class_path, file_name, "#{action}_view.rb"
+				)
+
+				template "view.rb.erb", @path
+			end
+		end
+
+		def add_routes
+			return if options[:skip_routes]
+
+			routing_code = "resources :#{file_name}"
+
+			if actions.any?
+				routing_code << ", only: [#{actions.map { ":#{_1}" }.join(', ')}]"
 			end
 
-			def copy_view_files
-				base_path = File.join("app/views", class_path, file_name)
-				empty_directory base_path
+			route routing_code, namespace: regular_class_path
+		end
 
-				actions.each do |action|
-					::Rails::Generators.invoke("phlex:page", [name + "/" + action])
-				end
-			end
+		hook_for :test_framework, as: :controller do |generator|
+			invoke generator, [remove_possible_suffix(name), actions]
+		end
 
-			def add_routes
-				return if options[:skip_routes]
+		private
 
-				routing_code = "resources :#{file_name}"
+		def parent_class_name
+			options[:parent]
+		end
 
-				if actions.any?
-					routing_code << ", only: [#{actions.map { ":#{_1}" }.join(', ')}]"
-				end
+		def file_name
+			remove_possible_suffix(super)
+		end
 
-				route routing_code, namespace: regular_class_path
-			end
+		def name
+			remove_possible_suffix(super)
+		end
 
-			hook_for :test_framework, as: :controller do |generator|
-				invoke generator, [remove_possible_suffix(name), actions]
-			end
-
-			private
-
-			def parent_class_name
-				options[:parent]
-			end
-
-			def file_name
-				remove_possible_suffix(super)
-			end
-
-			def name
-				remove_possible_suffix(super)
-			end
-
-			def remove_possible_suffix(name)
-				name.sub(/_?controller$/i, "")
-			end
+		def remove_possible_suffix(name)
+			name.sub(/_?controller$/i, "")
 		end
 	end
 end
