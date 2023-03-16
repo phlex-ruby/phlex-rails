135df0f1f6d12cd9e7dd46155bb11088d58379b7
Joel Drapper <joel@drapper.me>
2023-03-16 14:30:34 +0000

    Streaming

---
+++ lib/phlex/rails/streaming.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# @api experimental
+module Phlex::Rails::Streaming
+	private def stream(view)
+		headers.delete("Content-Length")
+
+		headers["X-Accel-Buffering"] = "no"
+		headers["Cache-Control"] = "no-cache"
+		headers["Content-Type"] = "text/html; charset=utf-8"
+		headers["Last-Modified"] = Time.zone.now.ctime.to_s
+
+		response.status = 200
+
+		self.response_body = Enumerator.new do |buffer|
+			view.call(buffer, view_context: view_context)
+		rescue
+			buffer << %('">)
+			buffer << view_context.javascript_tag(nonce: true) { %(window.location = "/500.html").html_safe }
+		end
+	end
+end
