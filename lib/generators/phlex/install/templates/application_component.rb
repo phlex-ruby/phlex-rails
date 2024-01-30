8fcbb4a337f420efc88efe53ab67478108ab4878
Brad Gessler <bradgessler@gmail.com>
2024-01-30 10:28:48 -0800

    Include all Rails helpers in Phlex generator

--- lib/generators/phlex/install/templates/application_component.rb
+++ lib/generators/phlex/install/templates/application_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationComponent < Phlex::HTML
-	include Phlex::Rails::Helpers::Routes
+	include Phlex::Rails::Helpers
 
 	if Rails.env.development?
 		def before_template
