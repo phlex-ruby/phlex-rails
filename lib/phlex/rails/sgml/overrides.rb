c9e095125f2a103ef70d3aab646a4362491d3fce
Brad Gessler <bradgessler@gmail.com>
2024-02-07 21:41:06 -0800

    Add render_1 and render_2 rendering methods

--- lib/phlex/rails/sgml/overrides.rb
+++ lib/phlex/rails/sgml/overrides.rb
@@ -4,30 +4,74 @@ module Phlex
 	module Rails
 		module SGML
 			module Overrides
+        def self.prepended(klass)
+          klass.alias_method :super_render, :render
+          klass.include RenderMethods
+          klass.extend ClassMethods
+          # Set the version to 1 by default.
+          klass.version 1
+        end
+
+        module RenderMethods
+          def render_1(*args, **kwargs, &block)
+            renderable = args[0]
+
+            warn "Phlex 2.0 will render #{renderable} using render_2" if renderable.is_a?(String)
+
+            case renderable
+            when Phlex::SGML, Proc
+              return super_render(*args, **kwargs, &block)
+            when Class
+              return super_render(*args, **kwargs, &block) if renderable < Phlex::SGML
+            when Enumerable
+              return super_render(*args, **kwargs, &block) unless renderable.is_a?(ActiveRecord::Relation)
+            else
+              captured_block = -> { capture(&block) } if block
+              @_context.target << @_view_context.render(*args, **kwargs, &captured_block)
+            end
+
+            nil
+          end
+
+          def render_2(*args, **kwargs, &block)
+            renderable = args[0]
+
+            case renderable
+            when Phlex::SGML, Proc, String
+              return super_render(*args, **kwargs, &block)
+            when Class
+              return super_render(*args, **kwargs, &block) if renderable < Phlex::SGML
+            when Enumerable
+              return super_render(*args, **kwargs, &block) unless renderable.is_a?(ActiveRecord::Relation)
+            else
+              captured_block = -> { capture(&block) } if block
+              @_context.target << @_view_context.render(*args, **kwargs, &captured_block)
+            end
+
+            nil
+          end
+        end
+
+        module ClassMethods
+          # Class method to switch render method implementations
+          def version(version)
+            case version
+            when 1
+              alias_method :render, :render_1
+            when 2
+              alias_method :render, :render_2
+            else
+              raise ArgumentError, "Unknown render method version: #{version}"
+            end
+          end
+        end
+
 				def helpers
 					if defined?(ViewComponent::Base) && @_view_context.is_a?(ViewComponent::Base)
 						@_view_context.helpers
 					else
 						@_view_context
 					end
-				end
-
-				def render(*args, **kwargs, &block)
-					renderable = args[0]
-
-					case renderable
-					when Phlex::SGML, Proc
-						return super
-					when Class
-						return super if renderable < Phlex::SGML
-					when Enumerable
-						return super unless renderable.is_a?(ActiveRecord::Relation)
-					else
-						captured_block = -> { capture(&block) } if block
-						@_context.target << @_view_context.render(*args, **kwargs, &captured_block)
-					end
-
-					nil
 				end
 
 				def render_in(view_context, &block)
--- spec/phlex/sgml_spec.rb
+++ spec/phlex/sgml_spec.rb
@@ -3,49 +3,123 @@
 require "spec_helper"
 
 RSpec.describe Phlex::SGML do
-	describe "#render" do
-		context "when given a block" do
-			it "renders content in the proper order" do
+	context "1.0" do
+		describe "#render" do
+			context "when given a block" do
+				it "renders content in the proper order" do
+					component = Class.new(Phlex::HTML) do
+						def template(&block)
+							plain "Component A\n"
+							render("examples/sgml/wrap", &block)
+							plain "Component B\n"
+						end
+					end
+
+					content = ExamplesController.render(inline: <<~ERB, locals: { component: component })
+						ERB A
+						<%= render component.new do %>
+							Hello
+						<% end %>
+						ERB B
+					ERB
+
+					expect(content).to eq(<<~OUTPUT)
+						ERB A
+						Component A
+						Partial A
+
+							Hello
+
+						Partial B
+						Component B
+						ERB B
+					OUTPUT
+				end
+			end
+
+			it "renders a template without a block" do
 				component = Class.new(Phlex::HTML) do
-					def template(&block)
-						plain "Component A\n"
-						render("examples/sgml/wrap", &block)
-						plain "Component B\n"
+					def template
+						render(template: "examples/sgml/template")
 					end
 				end
 
-				content = ExamplesController.render(inline: <<~ERB, locals: { component: component })
-					ERB A
-					<%= render component.new do %>
-						Hello
-					<% end %>
-					ERB B
-				ERB
+				content = ExamplesController.render(component)
 
-				expect(content).to eq(<<~OUTPUT)
-					ERB A
-					Component A
-					Partial A
+				expect(content).to eq("Plain template.")
+			end
 
-						Hello
+			it "does not render a string" do
+				component = Class.new(Phlex::HTML) do
+					def template
+						render("Plain string.")
+					end
+				end
 
-					Partial B
-					Component B
-					ERB B
-				OUTPUT
+				expect{ExamplesController.render(component)}.to raise_error(ActionView::MissingTemplate)
 			end
 		end
+	end
 
-		it "renders a template without a block" do
-			component = Class.new(Phlex::HTML) do
-				def template
-					render(template: "examples/sgml/template")
+	context "2.0" do
+		before { Phlex::SGML.version(2) }
+		after  { Phlex::SGML.version(1) }
+		describe "#render" do
+			context "when given a block" do
+				it "renders content in the proper order" do
+					component = Class.new(Phlex::HTML) do
+						def template(&block)
+							plain "Component A\n"
+							render(partial: "examples/sgml/wrap", &block)
+							plain "Component B\n"
+						end
+					end
+
+					content = ExamplesController.render(inline: <<~ERB, locals: { component: component })
+						ERB A
+						<%= render component.new do %>
+							Hello
+						<% end %>
+						ERB B
+					ERB
+
+					expect(content).to eq(<<~OUTPUT)
+						ERB A
+						Component A
+						Partial A
+
+							Hello
+
+						Partial B
+						Component B
+						ERB B
+					OUTPUT
 				end
 			end
 
-			content = ExamplesController.render(component)
+			it "renders a template without a block" do
+				component = Class.new(Phlex::HTML) do
+					def template
+						render(template: "examples/sgml/template")
+					end
+				end
 
-			expect(content).to eq("Plain template.")
+				content = ExamplesController.render(component)
+
+				expect(content).to eq("Plain template.")
+			end
+
+			it "renders a string" do
+				component = Class.new(Phlex::HTML) do
+					def template
+						render("Plain string.")
+					end
+				end
+
+				content = ExamplesController.render(component)
+
+				expect(content).to eq("Plain string.")
+			end
 		end
 	end
 end
