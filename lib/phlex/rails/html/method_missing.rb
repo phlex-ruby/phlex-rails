1662d1527811768e1aa26e25c0ad5e6ab42d186e
Joel Drapper <joel@drapper.me>
2024-03-17 18:26:21 +0000

    Raise helpful error when calling a missing rails helper (#166)
    * Raise helpful error when calling a missing rails helper
    
    Co-Authored-By: Seth Horsley <4959521+iseth@users.noreply.github.com>
    
    * Update method_missing.rb
    
    Co-Authored-By: Seth Horsley <4959521+iseth@users.noreply.github.com>
    
    * Update method_missing.rb
    
    Co-Authored-By: Seth Horsley <4959521+iseth@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Seth Horsley <4959521+iseth@users.noreply.github.com>

---
+++ lib/phlex/rails/html/method_missing.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Phlex::Rails::HTML::MethodMissing
+	def method_missing(name, *args, **kwargs, &block)
+		return super unless helpers.respond_to?(name)
+
+		const_name = name.to_s.gsub("?", "")
+		module_name = Phlex::Rails::Helpers.constants.find { |mod| mod.to_s.underscore.gsub("domid", "dom_id") == const_name }
+		return super unless module_name
+
+		raise NoMethodError, "Try including `Phlex::Rails::Helpers::#{module_name}` in #{self.class.name}."
+	end
+end
