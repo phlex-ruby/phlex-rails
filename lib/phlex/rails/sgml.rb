411e75394acc8b407b5562e42a1cc9ce21c0ebf4
Joel Moss <joel@developwithstyle.com>
2024-03-19 15:53:39 +0000

    fix: Guard for nil request when getting fragments
    fixes #170

--- lib/phlex/rails/sgml.rb
+++ lib/phlex/rails/sgml.rb
@@ -37,11 +37,13 @@ module Phlex
 				end
 
 				def render_in(view_context, &block)
-					fragments = if (fragment_header = view_context.request.headers["X-Fragment"])
-						fragment_header.split
-					elsif (turbo_frame = view_context.request.headers["Turbo-Frame"])
-						[turbo_frame]
-					end
+					fragments = if view_context.request
+												if (fragment_header = view_context.request.headers["X-Fragment"])
+													fragment_header.split
+												elsif (turbo_frame = view_context.request.headers["Turbo-Frame"])
+													[turbo_frame]
+												end
+											end
 
 					if block_given?
 						call(view_context: view_context, fragments: fragments) do |*args|
