1d152aa7b581e51fef5efc4d959dc70d84a74490
Joel Drapper <joel@drapper.me>
2024-03-23 11:02:37 +0000

    Update link_to.rb

--- lib/phlex/rails/helpers/link_to.rb
+++ lib/phlex/rails/helpers/link_to.rb
@@ -3,7 +3,21 @@
 module Phlex::Rails::Helpers::LinkTo
 	extend Phlex::Rails::HelperMacros
 
-	# @!method link_to(...)
-	# 	@return [nil]
-	register_output_helper :link_to
+	def link_to(*args, **kwargs, &block)
+		context = @_context
+
+		if context.fragments && !context.in_target_fragment && !context.fragments[kwargs[:id]]
+			return nil
+		end
+
+		output = if block
+			helpers.link_to(*args, **kwargs) { capture(&block) }
+		else
+			helpers.link_to(*args, **kwargs)
+		end
+
+		return unless ActiveSupport::SafeBuffer === output
+
+		context.target << output
+	end
 end
