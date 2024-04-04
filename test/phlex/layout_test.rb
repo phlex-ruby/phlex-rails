4c08a009d73e0bd94f51cebc217881f1a338dea0
Joel Drapper <joel@drapper.me>
2024-04-04 17:40:27 +0100

    Test layout param

--- test/phlex/layout_test.rb
+++ test/phlex/layout_test.rb
@@ -6,6 +6,7 @@ class LayoutTest < ActionDispatch::IntegrationTest
 	test "with erb view" do
 		get "/layout/with_erb_view"
 		assert_response :success
+		assert_select "body.bg-blue"
 		assert_select "title", "ERB View"
 		assert_select "h1", "Hello from ERB"
 	end
