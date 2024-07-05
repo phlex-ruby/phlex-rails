b6454030319846817feabfe7952d5e4677ebb12b
mhenrixon <mikael@mhenrixon.com>
2024-07-05 13:04:55 +0200

    feat: allow generators to skip suffix

--- lib/generators/phlex/component/component_generator.rb
+++ lib/generators/phlex/component/component_generator.rb
@@ -4,6 +4,8 @@ module Phlex::Generators
 	class ComponentGenerator < ::Rails::Generators::NamedBase
 		source_root File.expand_path("templates", __dir__)
 
+		class_option :suffix, type: :boolean, default: true, desc: "Set to false to avoid adding the 'Component' suffix to the class name."
+
 		def create_view
 			@path = File.join("app/views/components", class_path, "#{file_name}_component.rb")
 			template "component.rb.erb", @path
