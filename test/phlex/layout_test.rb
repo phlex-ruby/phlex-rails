32f6b516cb19bccfe6534a32fef25faa0b1d85d6
Matt Jankowski <matt@jankowski.online>
2024-08-17 15:11:59 -0400

    Fix `data` attribute in layout generator template

--- test/phlex/layout_test.rb
+++ test/phlex/layout_test.rb
@@ -7,6 +7,7 @@ class LayoutTest < ActionDispatch::IntegrationTest
 		get "/layout/with_erb_view"
 		assert_response :success
 		assert_select "body.bg-blue"
+		assert_select "link[rel=stylesheet][data-turbo-track=reload]"
 		assert_select "title", "ERB View"
 		assert_select "h1", "Hello from ERB"
 	end
@@ -14,6 +15,7 @@ class LayoutTest < ActionDispatch::IntegrationTest
 	test "with phlex view" do
 		get "/layout/with_phlex_view"
 		assert_response :success
+		assert_select "link[rel=stylesheet][data-turbo-track=reload]"
 		assert_select "title", "Phlex View"
 		assert_select "h1", "Hello from Phlex"
 	end
