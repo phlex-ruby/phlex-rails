af702ac7ad42974d47365cbfd9cd987d84876145
Joel Drapper <joel@drapper.me>
2024-09-07 15:08:18 +0100

    New testing module

---
+++ lib/phlex/rails/testing.rb
@@ -0,0 +1,17 @@
+
+require "phlex/rails"
+require "phlex/testing"
+
+module Phlex::Testing::RailsContext
+	def view_context
+		controller.view_context
+	end
+
+	def controller
+		@controller ||= ActionView::TestCase::TestController.new
+	end
+end
+
+Phlex::Testing::SGML.include(
+	Phlex::Testing::RailsContext
+)
