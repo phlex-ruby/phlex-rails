bea3d605553cc072bfebdecb1d1e3f7ffe09437d
Joel Drapper <joel@drapper.me>
2025-02-10 22:03:31 +0000

    Group the autoloads

--- lib/phlex/rails.rb
+++ lib/phlex/rails.rb
@@ -5,20 +5,25 @@ require "phlex"
 module Phlex
 	module Rails
 		autoload :Buffered, "phlex/rails/buffered"
+		autoload :Unbuffered, "phlex/rails/unbuffered"
+
 		autoload :BufferedCheckboxBuilder, "phlex/rails/buffered_checkbox_builder"
 		autoload :BufferedFormBuilder, "phlex/rails/buffered_form_builder"
 		autoload :BufferedLabelBuilder, "phlex/rails/buffered_label_builder"
 		autoload :BufferedRadioButtonBuilder, "phlex/rails/buffered_radio_button_builder"
-		autoload :CSV, "phlex/rails/csv"
+
+		autoload :SGML, "phlex/rails/sgml"
 		autoload :HTML, "phlex/rails/html"
-		autoload :HelperFinder, "phlex/rails/helper_finder"
-		autoload :HelperMacros, "phlex/rails/helper_macros"
+
+		autoload :CSV, "phlex/rails/csv"
+
 		autoload :Helpers, "phlex/rails/helpers"
+		autoload :HelperMacros, "phlex/rails/helper_macros"
+		autoload :HelperFinder, "phlex/rails/helper_finder"
+
 		autoload :Layout, "phlex/rails/layout"
 		autoload :Partial, "phlex/rails/partial"
-		autoload :SGML, "phlex/rails/sgml"
 		autoload :Streaming, "phlex/rails/streaming"
-		autoload :Unbuffered, "phlex/rails/unbuffered"
 	end
 
 	CSV.extend Phlex::Rails::HelperMacros
