e07ec6665f2c7505575f3a2836933fa148eaab20
Michael <mmoen@vivoaquatics.com>
2025-04-30 09:07:56 -0500

    Allow the use of custom generator tempaltes

--- lib/generators/phlex/component/component_generator.rb
+++ lib/generators/phlex/component/component_generator.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+require_relative "../base_generator"
+
 module Phlex::Generators
-	class ComponentGenerator < ::Rails::Generators::NamedBase
-		source_root File.expand_path("templates", __dir__)
+	class ComponentGenerator < BaseGenerator
+		set_source_root "component.rb.erb", __dir__
 
 		def create_component
 			template "component.rb.erb", File.join(
